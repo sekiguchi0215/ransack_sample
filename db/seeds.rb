c91e0fea398317b87de89c0c857a052c41728b73
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-06 00:05:50 +0900

    初期データを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+Faker::Config.locale = :ja
+
+1000.times do
+  User.create!(name: Faker::Name.name, age: rand(0..100))
+end
+
+puts "初期データの投入に成功しました！"
